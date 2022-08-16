2dea52f50aff2c99238088990c872587552548a2
jadsonmarliere <jds.mdo@gmail.com>
2022-08-16 14:56:39 -0300

    update ConsultasLinq

--- ConsultasLinq/Program.cs
+++ ConsultasLinq/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("----------- start -------------");
+
+            var query = from n in numeros
+                        select n;
+
+            foreach(var num in query)
+            {
+                Console.WriteLine("{0}\t{1}", 
+                    num.Id, num.numero);
+            }
+            Console.ReadKey();
         }
     }
 }
