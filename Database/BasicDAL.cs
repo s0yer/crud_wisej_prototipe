78dc68814eef3f96ec9fdc68a34e761dea882b95
jadson.oliveira <jds.mdo@gmail.com>
2022-10-17 15:35:18 -0300

    update
    string connection

--- Database/BasicDAL.cs
+++ Database/BasicDAL.cs
@@ -1,6 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Encoders;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
 using Wisej.Web;
 
 namespace crud_wisej_prototipe
@@ -8,16 +11,16 @@ namespace crud_wisej_prototipe
     public class BasicDAL
     {
 
-    private MySqlConnection conexao;               
+        private MySqlConnection conexao;
 
-    //Construtor que ja abre a conexao quando executa a aplicacao
-    public BasicDAL()
+        //Construtor que ja abre a conexao quando executa a aplicacao
+        public BasicDAL()
         {
             //criar um metodo para ver se o bd existe
 
             try
             {
-               // AbreConexao();
+                // AbreConexao();
                 //this.conexao = new mysqlconnection("server=localhost;database=mydb;uid=root;password=123456;port=3306;");
                 //this.conexao.open();
                 //messagebox.show("conexao aberta...");
@@ -33,9 +36,21 @@ namespace crud_wisej_prototipe
             }
         }
 
+        //public MySqlConnection AbreConexao(ConnectionStringSettings connectionString)
         public MySqlConnection AbreConexao()
         {
             this.conexao = new MySqlConnection("server=localhost;database=mydb;uid=root;password=123456;port=3306;");
+
+
+            //public static byte[] strConexao = Convert.FromBase64String("c2VydmVyPWxvY2FsaG9zdDtkYXRhYmFzZT1teWRiO3VpZD1yb290O3Bhc3N3b3JkPTEyMzQ1Njtwb3J0PTMzMDY7");
+            //this.conexao = new MySqlConnection(Encoding(strConexao));
+
+            //string connString = ConfigurationManager.ConnectionStrings[“database1”].ConnectionString;
+            //this.conexao = new MySqlConnection("stringconnection?");
+            //root@localhost:3306
+            //jdbc:mysql://localhost:3306/?user=root
+            //jdbc:mysql://localhost:3306/?user=root
+
             this.conexao.Open();
             //MessageBox.Show("Conexao aberta...");
             Console.WriteLine("Conexao aberta...");
@@ -43,92 +58,92 @@ namespace crud_wisej_prototipe
             return this.conexao;
         }
 
-        public bool EstadoConexao(MySqlConnection conexao)
+    public bool EstadoConexao(MySqlConnection conexao)
+    {
+        if (conexao.State == System.Data.ConnectionState.Open)
         {
-            if(conexao.State == System.Data.ConnectionState.Open)
-            {
-                return true;
-            }
-            else { return false; }
-            
+            return true;
         }
+        else { return false; }
+
+    }
 
 
-        public void Dispose()
-        {
-            this.conexao.Close();
-            this.conexao.Dispose();
-        }
+    public void Dispose()
+    {
+        this.conexao.Close();
+        this.conexao.Dispose();
+    }
 
     internal void Adicionar(Luck lu)
+    {
+        try
         {
-            try
-            {
-                //var conexao = AbreConexao();
+            //var conexao = AbreConexao();
 
-                var insertCmd = AbreConexao().CreateCommand();
-                insertCmd.CommandText = "INSERT INTO lucknumber (numero) VALUES (@numero)";
+            var insertCmd = AbreConexao().CreateCommand();
+            insertCmd.CommandText = "INSERT INTO lucknumber (numero) VALUES (@numero)";
 
-                var paramNumero = new MySqlParameter("numero", lu.numero);
-                insertCmd.Parameters.Add(paramNumero);
+            var paramNumero = new MySqlParameter("numero", lu.numero);
+            insertCmd.Parameters.Add(paramNumero);
 
-                //conexao.Close();
-                Dispose();
-            }
-            catch (MySqlException e)
-            {
-                throw new SystemException(e.Message, e);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }            
+            //conexao.Close();
+            Dispose();
         }
+        catch (MySqlException e)
+        {
+            throw new SystemException(e.Message, e);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 
     internal void Atualizar(Luck lu)
+    {
+        try
         {
-            try
-            {
-                var updateCmd = conexao.CreateCommand();
-                updateCmd.CommandText = "UPDATE lucknumber SET numero = @numero WHERE Id = @id";
+            var updateCmd = conexao.CreateCommand();
+            updateCmd.CommandText = "UPDATE lucknumber SET numero = @numero WHERE Id = @id";
 
-                var paraNumero = new MySqlParameter("numero", lu.numero);
-            }
-            catch (MySqlException e)
-            {
-                throw new SystemException(e.Message, e);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var paraNumero = new MySqlParameter("numero", lu.numero);
         }
+        catch (MySqlException e)
+        {
+            throw new SystemException(e.Message, e);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 
 
     internal void Remover(Luck lu)
+    {
+        try
         {
-            try
-            {
-                var deleteCmd = conexao.CreateCommand();
-                deleteCmd.CommandText = "DELETE FROM lucknum WHERE Id = @id";
+            var deleteCmd = conexao.CreateCommand();
+            deleteCmd.CommandText = "DELETE FROM lucknum WHERE Id = @id";
 
-                var paraNumero = new MySqlParameter("id", lu.Id);
-            }
-            catch (MySqlException e)
-            {
-                throw new SystemException(e.Message, e);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var paraNumero = new MySqlParameter("id", lu.Id);
         }
-
-
-
-        // metodos antigos --------------------------
-
-
+        catch (MySqlException e)
+        {
+            throw new SystemException(e.Message, e);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
+
+
+
+    // metodos antigos --------------------------
+
+
+}
 
 }
